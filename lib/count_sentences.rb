f4232cadd97c1ab150f16d87f067913f34906f90
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-12 22:18:08 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,19 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject {|string| string.empty?}.size
   end
+
 end
